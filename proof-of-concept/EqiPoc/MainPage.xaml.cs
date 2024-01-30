97329d26924301be195dfca229f665045e829c83
James Hughes <james@pyrosoftsolutions.co.uk>
2024-01-30 20:51:46 +0000

    Created start/stop buttons

--- proof-of-concept/EqiPoc/MainPage.xaml.cs
+++ proof-of-concept/EqiPoc/MainPage.xaml.cs
@@ -2,23 +2,20 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnStartTreadmill(object sender, EventArgs e)
 	{
-		count++;
+		Console.WriteLine("Starting Treadmill");
+	}
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+	private void OnStopTreadmill(object sender, EventArgs e)
+	{
+		Console.WriteLine("Stopping Treadmill");
 	}
 }
 
