4dbcc5cbc9591956c6f64c06919accdd0b4a9fd4
James Hughes <james@pyrosoftsolutions.co.uk>
2024-01-30 21:44:30 +0000

    We can find the treadmill!

--- proof-of-concept/EqiPoc/MainPage.xaml.cs
+++ proof-of-concept/EqiPoc/MainPage.xaml.cs
@@ -1,11 +1,35 @@
-﻿namespace EqiPoc;
+﻿using System.Diagnostics;
+using System.Reflection.Metadata;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace EqiPoc;
 
 public partial class MainPage : ContentPage
 {
-
+	private const string DeviceName = "CITYSPORTS-Linker";
+	private readonly IAdapter _adapter;
+	private IDevice _device;
+	
 	public MainPage()
 	{
+		_adapter = CrossBluetoothLE.Current.Adapter;
 		InitializeComponent();
+	}
+
+	protected override void OnAppearing()
+	{
+		BluetoothStatusLbl.Text = "Searching for Bluetooth Device";
+		_adapter.StartScanningForDevicesAsync();
+		_adapter.DeviceDiscovered += (_, e) =>
+		{
+			if (e.Device.Name == DeviceName)
+			{
+				_device = e.Device;
+				BluetoothStatusLbl.Text = $"Device Found: {_device.Name}\n[{_device.Id}]"; 
+				_adapter.StopScanningForDevicesAsync();
+			}
+		};
 	}
 
 	private void OnStartTreadmill(object sender, EventArgs e)
