353dbf0a2fba28194129da3ba8e970a63eb39ea2
James Hughes <james@pyrosoftsolutions.co.uk>
2024-02-01 21:55:10 +0000

    Created skeleton for new solution

---
+++ src/OpenEqiSports/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OpenEqiSports;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
