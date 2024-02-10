10db4c309a391d2aff2623eead11b0fb48158853
James Hughes <james@pyrosoftsolutions.co.uk>
2024-02-10 21:37:32 +0000

    Added new MAUI blazor project

---
+++ src/OpenEqiSports/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenEqiSports;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ src/OpenEqiSports/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenEqiSports;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
