4f01c5ba543572937f9f5ab3407c360fc8496545
James Hughes <james@pyrosoftsolutions.co.uk>
2024-03-30 00:32:27 +0000

    Dramatically simplified parsing
    Signed-off-by: James Hughes <james@Jamess-MBP.tasman.local>

--- src/lib/modules/demo_ctrl/treadmill_ctrl/treadmill_status.dart
+++ src/lib/modules/demo_ctrl/treadmill_ctrl/treadmill_status.dart
@@ -16,36 +16,14 @@ class WorkoutStatus {
   });
 
   factory WorkoutStatus.fromBytes(List<int> value) {
-    Uint16List status = Uint16List.fromList(value);
-    print("uint16: ${status.map((e) => e.toRadixString(16)).join(" ")}");
+    Uint8List status = Uint8List.fromList(value);
+    ByteData statusBytes = status.buffer.asByteData();
 
-    ByteData speedBuffer = ByteData(2)
-      ..setUint8(0, status[2])
-      ..setUint8(1, status[3]);
-
-    var speed = speedBuffer.getUint16(0, Endian.little) / 100;
-
-    print("${_byteDataAsHex(speedBuffer)} -> $speed km/h");
-
-    ByteData distanceBuffer = ByteData(2)
-      ..setUint8(0, status[4])
-      ..setUint8(1, status[5]);
-    var distance = distanceBuffer.getUint16(0, Endian.little) / 100;
-
-    ByteData timeBuffer = ByteData(2)
-      ..setUint8(0, status[12])
-      ..setUint8(1, status[13]);
-    var time = timeBuffer.getUint16(0, Endian.little);
-
-    ByteData stepsBuffer = ByteData(2)
-      ..setUint8(0, status[14])
-      ..setUint8(1, status[15]);
-    var steps = stepsBuffer.getUint16(0, Endian.little);
-
-    ByteData caloriesBuffer = ByteData(2)
-      ..setUint8(0, status[6])
-      ..setUint8(1, status[7]);
-    var calories = caloriesBuffer.getUint16(0, Endian.big);
+    var speed = statusBytes.getUint16(2, Endian.little) / 100;
+    var distance = statusBytes.getUint16(4, Endian.little) / 1000;
+    var time = statusBytes.getUint16(12, Endian.little);
+    var steps = statusBytes.getUint16(14, Endian.little);
+    var calories = statusBytes.getUint16(6, Endian.big);
 
     return WorkoutStatus(
       speedInKmh: speed,
@@ -54,18 +32,5 @@ class WorkoutStatus {
       indicatedCalories: calories,
       steps: steps,
     );
-  }
-
-  static String _byteDataAsHex(ByteData byteData) {
-    List<int> bytes = byteData.buffer.asUint8List();
-    return bytes.map((byte) => byte.toRadixString(16).padLeft(2, '0')).join(' ');
-  }
-
-  void prettyPrint() {
-    print('Speed: $speedInKmh km/h');
-    print('Distance: $distanceInKm km');
-    print('Time: $timeInSeconds seconds');
-    print('Calories: $indicatedCalories kcal');
-    print('Steps: $steps');
   }
 }
