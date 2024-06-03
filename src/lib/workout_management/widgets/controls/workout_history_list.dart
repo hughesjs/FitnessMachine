64432d5220b7676c424790a107b49b943ce1199e
James Hughes <james@pyrosoftsolutions.co.uk>
2024-06-03 23:01:45 +0100

    Created slightly better UI for workout history list

--- src/lib/workout_management/widgets/controls/workout_history_list.dart
+++ src/lib/workout_management/widgets/controls/workout_history_list.dart
@@ -1,31 +1,44 @@
 import 'package:fitness_machine/workout_management/models/completed_workout.dart';
 import 'package:fitness_machine/workout_management/widgets/cubits/workout_history_cubit.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
-import 'package:logger/logger.dart';
+import 'package:intl/intl.dart';
 
 class WorkoutHistoryList extends StatelessWidget {
-  final Logger _logger;
-
-  WorkoutHistoryList({super.key}): _logger = GetIt.I<Logger>();
+  const WorkoutHistoryList({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider<WorkoutHistoryCubit>(
-          create: (context) => WorkoutHistoryCubit(),
-          child: BlocBuilder<WorkoutHistoryCubit, List<CompletedWorkout>>(builder: (context, state) {
-            return ListView.builder(
-                itemCount: state.length,
-                itemBuilder: (context, index) {
-                  final workout = state[index];
-                  return ListTile(
-                      title: Text(workout.startedAt.toString()),
-                      subtitle: Text("${workout.distanceInKm}km in ${workout.workoutTimeInSeconds}s"),
-                      onTap: () {
-                        _logger.i("Tapped workout ${workout.workoutId}... It wasn't very effective");
-                      });
-                });
-          }));
+        create: (context) => WorkoutHistoryCubit(),
+        child: BlocBuilder<WorkoutHistoryCubit, List<CompletedWorkout>>(builder: (context, state) {
+          return ListView.builder(
+              itemCount: state.length,
+              itemBuilder: (context, index) {
+                final workout = state[index];
+                return Container(
+                    padding: const EdgeInsets.all(5),
+                    child: Card( 
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(children: [
+                      Row(
+                        children: [
+                          Expanded(child: Text(DateFormat("dd-MM-yyyy  HH:mm").format(workout.startedAt))),
+                          Text("${workout.workoutTimeInSeconds / 60} mins")
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Align(alignment: Alignment.centerLeft, child: Text("${workout.distanceInKm} km")),
+                          Expanded(child: Text("${workout.totalSteps} steps")),
+                          Align(alignment: Alignment.centerRight, child: Text("${workout.machineIndicatedCalories} kcal"))
+                        ],
+                      ),
+                    ]))));
+              });
+        }));
   }
 }
