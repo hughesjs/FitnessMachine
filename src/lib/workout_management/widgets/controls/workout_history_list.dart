afdbc7485a77163bc4c3aee749e521dc0adfb7e9
James Hughes <james@pyrosoftsolutions.co.uk>
2024-06-04 22:30:32 +0100

    Refactored workout history page

--- src/lib/workout_management/widgets/controls/workout_history_list.dart
+++ src/lib/workout_management/widgets/controls/workout_history_list.dart
@@ -1,8 +1,8 @@
 import 'package:fitness_machine/workout_management/models/completed_workout.dart';
+import 'package:fitness_machine/workout_management/widgets/controls/workout_history_list_item.dart';
 import 'package:fitness_machine/workout_management/widgets/cubits/workout_history_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 
 class WorkoutHistoryList extends StatelessWidget {
   const WorkoutHistoryList({super.key});
@@ -17,39 +17,7 @@ class WorkoutHistoryList extends StatelessWidget {
             itemCount: state.length,
             itemBuilder: (context, index) {
               final workout = state[index];
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  elevation: 3,
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            _textWithIcon(DateFormat("dd-MM-yyyy  HH:mm").format(workout.startedAt), const Icon(Icons.calendar_month)),
-                            _textWithIcon("${(workout.workoutTimeInSeconds / 60).toStringAsFixed(1)} mins", const Icon(Icons.timer)),
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            _textWithIcon("${workout.distanceInKm.toStringAsFixed(2)} km", const Icon(Icons.place)),
-                            _textWithIcon("${workout.totalSteps} steps", const Icon(Icons.directions_walk)),
-                            _textWithIcon("${workout.machineIndicatedCalories} kcal", const Icon(Icons.local_fire_department)),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
+              return WorkoutHistoryListItem(workout);
             },
           );
         },
@@ -57,13 +25,5 @@ class WorkoutHistoryList extends StatelessWidget {
     );
   }
 
-  Widget _textWithIcon(String text, Icon icon) {
-    return Row(
-      children: [
-        icon,
-        const SizedBox(width: 5),
-        Text(text),
-      ],
-    );
-  }
+
 }
