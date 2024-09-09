fa1b1ecbd912a181b7ff50dedca7a3b2ec183bf4
cschmollJambit <christian.schmoll@jambit.com>
2024-09-09 18:56:17 +0200

    Resize current speed, Relocate min/max speed, Add unit

--- src/lib/treadmill_control/widgets/controls/speed_indicator.dart
+++ src/lib/treadmill_control/widgets/controls/speed_indicator.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fitness_machine/treadmill_control/models/speed_state.dart';
 import 'package:fitness_machine/treadmill_control/widgets/cubits/speed_range_and_setting_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SpeedIndicator extends StatelessWidget {
   const SpeedIndicator({super.key});
@@ -18,23 +18,54 @@ class SpeedIndicator extends StatelessWidget {
               children: [
                 Padding(
                     padding: const EdgeInsets.all(8),
-                    child: ElevatedButton(onPressed: () async => cubit.speedDown(), child: const Icon(Icons.arrow_downward))),
+                    child: ElevatedButton(
+                        onPressed: () async => cubit.speedDown(),
+                        child: const Icon(Icons.remove))),
                 Expanded(
-                    child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-                  Row(children: [
-                    Align(alignment: Alignment.centerLeft, child: Text(state.minSpeed.toStringAsFixed(1))),
-                    Expanded(child: Align(alignment: Alignment.center, child: Text(state.speedInKmh.toStringAsFixed(1)))),
-                    Align(alignment: Alignment.centerRight, child: Text(state.maxSpeed.toStringAsFixed(1)))
-                  ]),
-                  Row(children: [
-                    Expanded(
-                        child: LinearProgressIndicator(
-                            value: state.maxSpeed > 0 ? (state.speedInKmh - state.minSpeed) / (state.maxSpeed - state.minSpeed) : 0))
-                  ]),
-                ])),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                      Row(children: [
+                        Expanded(
+                            child: Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  state.speedInKmh.toStringAsFixed(1),
+                                  style:
+                                      Theme.of(context).textTheme.displayMedium,
+                                ))),
+                      ]),
+                      Row(children: [
+                        Expanded(
+                            child: LinearProgressIndicator(
+                                value: state.maxSpeed > 0
+                                    ? (state.speedInKmh - state.minSpeed) /
+                                        (state.maxSpeed - state.minSpeed)
+                                    : 0))
+                      ]),
+                      Row(
+                        children: [
+                          Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(state.minSpeed.toStringAsFixed(1))),
+                          Expanded(
+                              child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text("km/h",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .labelLarge))),
+                          Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(state.maxSpeed.toStringAsFixed(1))),
+                        ],
+                      )
+                    ])),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(onPressed: () async => cubit.speedUp(), child: const Icon(Icons.arrow_upward)),
+                  child: ElevatedButton(
+                      onPressed: () async => cubit.speedUp(),
+                      child: const Icon(Icons.add)),
                 ),
               ],
             ));
